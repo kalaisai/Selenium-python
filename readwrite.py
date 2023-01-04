4b94dd4cb61a0b047cb4a5a9cf6fb0b3b980b486
kalaisai <119919923+kalaisai@users.noreply.github.com>
2023-01-04 17:44:11 +0530

    commit new

--- readwrite.py
+++ readwrite.py
@@ -1,14 +1,14 @@
 file = open('read')
 
-#print(file.read())
-#file.close()
+print(file.read())
+file.close()
 
-#line = file.readline()
-#while line !="":
-#    print(line)
- #   line=file.readline()
+line = file.readline()
+while line !="":
+    print(line)
+    line=file.readline()
 
-
+####
 for line in file.readlines():
     print(line)
 
